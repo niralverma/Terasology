504c175daadd1c9c3affe5177cc199c9ab6678ba
Tobias Nett <skaldarnar@googlemail.com>
2020-12-06 13:00:55 +0100

    test: add test for BlockRegion#center()

--- engine-tests/src/test/java/org/terasology/math/BlockRegionTest.java
+++ engine-tests/src/test/java/org/terasology/math/BlockRegionTest.java
@@ -17,15 +17,21 @@
 package org.terasology.math;
 
 import com.google.common.collect.Sets;
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
 import org.joml.Vector3i;
 import org.joml.Vector3ic;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.terasology.world.block.BlockRegion;
 import org.terasology.world.block.BlockRegions;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -169,10 +175,58 @@ public class BlockRegionTest {
     public void testCorrectBoundsMixed() {
         Vector3i min = new Vector3i(0, 0, 0);
         Vector3i max = new Vector3i(1, 1, 1);
-        BlockRegion region = BlockRegions.createFromMinAndMax(1,0,1, 0,1,0);
+        BlockRegion region = BlockRegions.createFromMinAndMax(1, 0, 1, 0, 1, 0);
         region.correctBounds();
 
         assertEquals(min, region.getMin(new Vector3i()));
         assertEquals(max, region.getMax(new Vector3i()));
+    }
+
+    private static Stream<Arguments> testCenterArgs() {
+        return Stream.of(
+                Arguments.of(
+                        new BlockRegion(),
+                        new Vector3f(Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE)
+                ),
+                // creating from min and max
+                Arguments.of(
+                        BlockRegions.createFromMinAndMax(new Vector3i(0, 0, 0), new Vector3i(0, 0, 0)),
+                        new Vector3f(0.5f, 0.5f, 0.5f)
+                ),
+                Arguments.of(
+                        BlockRegions.createFromMinAndMax(new Vector3i(0, 0, 0), new Vector3i(1, 1, 1)),
+                        new Vector3f(1f, 1f, 1f)
+                ),
+                Arguments.of(
+                        BlockRegions.createFromMinAndMax(new Vector3i(-1, -1, -1), new Vector3i(1, 1, 1)),
+                        new Vector3f(0.5f, 0.5f, 0.5f)
+                ),
+                Arguments.of(
+                        BlockRegions.createFromMinAndMax(new Vector3i(0, 0, 0), new Vector3i(2, 2, 2)),
+                        new Vector3f(1.5f, 1.5f, 1.5f)
+                ),
+                // creating from center and extents
+                Arguments.of(
+                        BlockRegions.createFromCenterAndExtents(new Vector3f(0, 0, 0), new Vector3f(0, 0, 0)),
+                        new Vector3f(0.5f, 0.5f, 0.5f)
+                ),
+                Arguments.of(
+                        BlockRegions.createFromCenterAndExtents(new Vector3f(0.5f, 0.5f, 0.5f), new Vector3f(0.5f,
+                                0.5f, 0.5f)),
+                        new Vector3f(1f, 1f, 1f)
+                ),
+                Arguments.of(
+                        BlockRegions.createFromCenterAndExtents(new Vector3f(0.49f, 0.49f, 0.49f), new Vector3f(0.5f,
+                                0.5f, 0.5f)),
+                        new Vector3f(.5f, .5f, .5f)
+                )
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCenterArgs")
+    public void testCenter(BlockRegion region, Vector3fc expectedCenter) {
+        assertEquals(expectedCenter, region.center(new Vector3f()));
     }
 }
